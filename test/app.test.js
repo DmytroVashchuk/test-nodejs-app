1093d4bc65fa66d3e57afee4bb4620ba5535c79f
DmytroVashchuk <dimonchik06@gmail.com>
2024-10-10 18:00:06 +0000

    Change some files

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+// test/app.test.js
+const assert = require('chai').assert;
+
+describe('Basic Mocha Test', function() {
+  it('should return true', function() {
+    assert.equal(true, true);
+  });
+});
